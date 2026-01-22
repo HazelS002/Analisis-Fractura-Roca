668942b41f61d2a8b7b3822faa0f8f45a979b291
HazelS002 <hazelshamedsan@gmail.com>
2026-01-21 22:35:20 -0600

    Updated: show_images function

--- src/visualitation.py
+++ src/visualitation.py
@@ -2,26 +2,24 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def show_images(images:list, names:list[str], show:bool=True):
-    """ Muestra una lista de imagenes. """
+def show_images(images: list, names: list[str], show: bool = True):
+    """Muestra una lista de imágenes."""
     n_images = len(images)
+    n_cols = int(np.ceil(np.sqrt(n_images)))
+    n_rows = int(np.ceil(n_images / n_cols))
     
-    n_cols = np.ceil(np.sqrt(n_images)).astype(int)
-    n_rows = np.ceil(n_images / n_cols).astype(int)
-
-    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols)
-
+    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, squeeze=False)
+    
     for i, (img, name) in enumerate(zip(images, names)):
         r, c = i // n_cols, i % n_cols
         ax = axes[r, c]
-        ax.imshow(img, cmap="gray")
-        ax.set_title(name) ; ax.axis("off")
-
-    # apagar ejes vacíos
-    for j in range(i+1, n_cols * n_rows):
+        ax.imshow(img, cmap="gray") ; ax.set_title(name)
+        ax.axis("off")
+    
+    for j in range(i + 1, n_cols * n_rows):    # Apagar ejes vacíos
         r, c = j // n_cols, j % n_cols
         axes[r, c].axis("off")
-
+    
     if show: plt.show()
     return fig, axes
 
