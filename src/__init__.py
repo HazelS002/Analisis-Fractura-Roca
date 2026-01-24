ec8397d7ac73c9480eec6c77fab4ff033ee4bf84
HazelS002 <hazelshamedsan@gmail.com>
2026-01-24 15:42:51 -0600

    Proving metrics

--- src/__init__.py
+++ src/__init__.py
@@ -16,3 +16,14 @@ IMAGE_SIZE = (358, 232)  # (width, height)
 
 # tamaño para imagenes rotadas y traslatadas
 TRANSFORMATION_MARGIN = (400, 400)  # (width, height)
+
+
+NMF_PARAMS = {
+    # "n_components": 4, # El numero de componentes se agrega directamente
+    "init": "random",
+    "random_state": 42,
+    "max_iter": 500,
+    "solver": "mu",
+    "beta_loss": "kullback-leibler",
+    "verbose": 0
+}
--- src/analysis.py
+++ src/analysis.py
@@ -11,6 +11,7 @@ def apply_nmf(images: list[np.ndarray], **nmf_params)\
 
     # Aplicar NMF
     nmf_model = NMF(**nmf_params)
+    print(f"Fitting NMF model, {nmf_params['n_components']} components...")
     W = nmf_model.fit_transform(data_matrix)
     H = nmf_model.components_
 
@@ -22,13 +23,14 @@ def apply_nmf(images: list[np.ndarray], **nmf_params)\
 
 
 if __name__ == "__main__":
-    from src import SAMPLE_DATA_DIR
+    from src import SAMPLE_DATA_DIR, NMF_PARAMS
     from src.load_images import read_images
     from src.visualitation import show_images, show_nmf_components,\
         plot_reconstruction_error
     from src.transform import clean_images
     from src.utils import get_stage_images
 
+    
     # leer y limpiar imagenes
     images, names = read_images(SAMPLE_DATA_DIR + "aligned-images/")
     images = get_stage_images(clean_images(images), "cleaned")
@@ -37,14 +39,13 @@ if __name__ == "__main__":
     show_images([np.mean(images, axis=0).astype(np.uint8)], ["Average Image"])
 
     # Evaluar error de reconstrucción para diferentes números de componentes
-    errors = [ apply_nmf(images, n_components=k, max_iter=500,
-                         random_state=42)[3] for k in range(1, 12) ]
+    errors = [ apply_nmf(images, n_components=k, **NMF_PARAMS)[3]\
+              for k in range(1, 12) ]
     plot_reconstruction_error(errors, list(range(1, 12)))
 
     # aplicar NMF
-    for k in [2, 6]:
+    for k in [2, 10]:
         print(f"\nNMF con {k} componentes...")
-        nmf_images, W, H, _ = apply_nmf(images, n_components=k, max_iter=500,
-                                        random_state=42)
-        show_images(nmf_images, names)
+        nmf_images, W, H, _ = apply_nmf(images, n_components=k, **NMF_PARAMS)
         show_nmf_components(H, W, images[0].shape)
+        show_images(nmf_images, names)
