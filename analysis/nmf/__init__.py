79c7fd0b49075b8315a8f4bc6bd5845082734d7b
HazelS002 <hazelshamedsan@gmail.com>
2026-08-12 01:27:07 -0600

    Delated nmf implementation

--- analysis/nmf/__init__.py
+++
@@ -1,5 +0,0 @@
-from .solver import apply_nmf
-
-__all__ = [
-    "apply_nmf"
-]
--- analysis/nmf/config.py
+++
@@ -1,9 +0,0 @@
-nmf_kwargs = {    # paramestros de NMF
-    # "n_components": 4,  # el numero de componentes se maneja directamente
-    "init": "random",
-    "random_state": 42,
-    "max_iter": 100,
-    "solver": "mu",
-    "beta_loss": "kullback-leibler",
-    "verbose": True
-}
